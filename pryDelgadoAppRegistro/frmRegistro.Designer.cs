2bef38a906e8e7b3641c5b153d0923d2937700cd
delgadosofiaguada-jpg <delgado.sofiaguada@gmail.com>
2025-09-15 23:55:15 -0300

    terminado

--- pryDelgadoAppRegistro/frmRegistro.Designer.cs
+++ pryDelgadoAppRegistro/frmRegistro.Designer.cs
@@ -35,18 +35,21 @@
             lblTitulo = new Label();
             dtpFecha = new DateTimePicker();
             lstProductos = new ComboBox();
-            txtPrecioUnitario = new TextBox();
             cmdAceptar = new Button();
-            txtCantidad = new TextBox();
+            nudCantidad = new NumericUpDown();
+            mtbPrecio = new MaskedTextBox();
+            lblResultados = new Label();
+            cmdCancelar = new Button();
+            ((System.ComponentModel.ISupportInitialize)nudCantidad).BeginInit();
             SuspendLayout();
             // 
             // lblFecha
             // 
             lblFecha.AutoSize = true;
             lblFecha.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lblFecha.Location = new Point(116, 82);
+            lblFecha.Location = new Point(51, 122);
             lblFecha.Name = "lblFecha";
-            lblFecha.Size = new Size(50, 21);
+            lblFecha.Size = new Size(62, 28);
             lblFecha.TabIndex = 0;
             lblFecha.Text = "Fecha";
             // 
@@ -54,9 +57,9 @@
             // 
             lblPrecioUnitario.AutoSize = true;
             lblPrecioUnitario.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lblPrecioUnitario.Location = new Point(116, 274);
+            lblPrecioUnitario.Location = new Point(51, 378);
             lblPrecioUnitario.Name = "lblPrecioUnitario";
-            lblPrecioUnitario.Size = new Size(107, 20);
+            lblPrecioUnitario.Size = new Size(138, 25);
             lblPrecioUnitario.TabIndex = 1;
             lblPrecioUnitario.Text = "Precio Unitario";
             // 
@@ -64,9 +67,9 @@
             // 
             lblCantidad.AutoSize = true;
             lblCantidad.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lblCantidad.Location = new Point(116, 208);
+            lblCantidad.Location = new Point(51, 290);
             lblCantidad.Name = "lblCantidad";
-            lblCantidad.Size = new Size(69, 20);
+            lblCantidad.Size = new Size(88, 25);
             lblCantidad.TabIndex = 2;
             lblCantidad.Text = "Cantidad";
             // 
@@ -74,9 +77,9 @@
             // 
             lblProducto.AutoSize = true;
             lblProducto.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lblProducto.Location = new Point(116, 146);
+            lblProducto.Location = new Point(51, 208);
             lblProducto.Name = "lblProducto";
-            lblProducto.Size = new Size(69, 20);
+            lblProducto.Size = new Size(89, 25);
             lblProducto.TabIndex = 3;
             lblProducto.Text = "Producto";
             // 
@@ -84,61 +87,92 @@
             // 
             lblTitulo.AutoSize = true;
             lblTitulo.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lblTitulo.Location = new Point(295, 18);
+            lblTitulo.Location = new Point(51, 34);
             lblTitulo.Name = "lblTitulo";
-            lblTitulo.Size = new Size(144, 21);
+            lblTitulo.Size = new Size(180, 28);
             lblTitulo.TabIndex = 4;
             lblTitulo.Text = "Registro de Venta";
-            
             // 
             // dtpFecha
             // 
             dtpFecha.CalendarFont = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            dtpFecha.Location = new Point(260, 82);
+            dtpFecha.Location = new Point(215, 122);
+            dtpFecha.Margin = new Padding(3, 4, 3, 4);
             dtpFecha.Name = "dtpFecha";
-            dtpFecha.Size = new Size(226, 23);
+            dtpFecha.Size = new Size(273, 27);
             dtpFecha.TabIndex = 5;
             // 
             // lstProductos
             // 
             lstProductos.FormattingEnabled = true;
-            lstProductos.Location = new Point(260, 149);
+            lstProductos.Items.AddRange(new object[] { "Monitor ", "Teclado", "Unidad Central de Procesamiento (CPU)", "Placa base", "Memoria RAM", "Altavoces" });
+            lstProductos.Location = new Point(215, 212);
+            lstProductos.Margin = new Padding(3, 4, 3, 4);
             lstProductos.Name = "lstProductos";
-            lstProductos.Size = new Size(226, 23);
+            lstProductos.Size = new Size(258, 28);
             lstProductos.TabIndex = 6;
-            // 
-            // txtPrecioUnitario
-            // 
-            txtPrecioUnitario.Location = new Point(260, 271);
-            txtPrecioUnitario.Name = "txtPrecioUnitario";
-            txtPrecioUnitario.Size = new Size(223, 23);
-            txtPrecioUnitario.TabIndex = 8;
+            lstProductos.SelectedIndexChanged += lstProductos_SelectedIndexChanged;
             // 
             // cmdAceptar
             // 
             cmdAceptar.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            cmdAceptar.Location = new Point(116, 360);
+            cmdAceptar.Location = new Point(51, 464);
+            cmdAceptar.Margin = new Padding(3, 4, 3, 4);
             cmdAceptar.Name = "cmdAceptar";
-            cmdAceptar.Size = new Size(96, 30);
+            cmdAceptar.Size = new Size(110, 40);
             cmdAceptar.TabIndex = 9;
             cmdAceptar.Text = "ACEPTAR";
             cmdAceptar.UseVisualStyleBackColor = true;
+            cmdAceptar.Click += cmdAceptar_Click;
             // 
-            // txtCantidad
+            // nudCantidad
             // 
-            txtCantidad.Location = new Point(263, 211);
-            txtCantidad.Name = "txtCantidad";
-            txtCantidad.Size = new Size(223, 23);
-            txtCantidad.TabIndex = 10;
+            nudCantidad.Location = new Point(218, 289);
+            nudCantidad.Name = "nudCantidad";
+            nudCantidad.Size = new Size(255, 27);
+            nudCantidad.TabIndex = 10;
+            nudCantidad.ValueChanged += nudCantidad_ValueChanged;
+            // 
+            // mtbPrecio
+            // 
+            mtbPrecio.Location = new Point(218, 376);
+            mtbPrecio.Mask = "99999$";
+            mtbPrecio.Name = "mtbPrecio";
+            mtbPrecio.Size = new Size(255, 27);
+            mtbPrecio.TabIndex = 11;
+            mtbPrecio.ValidatingType = typeof(int);
+            mtbPrecio.MaskInputRejected += mtbPrecio_MaskInputRejected;
+            // 
+            // lblResultados
+            // 
+            lblResultados.AutoSize = true;
+            lblResultados.Location = new Point(145, 534);
+            lblResultados.Name = "lblResultados";
+            lblResultados.Size = new Size(0, 20);
+            lblResultados.TabIndex = 12;
+            // 
+            // cmdCancelar
+            // 
+            cmdCancelar.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            cmdCancelar.Location = new Point(218, 464);
+            cmdCancelar.Margin = new Padding(3, 4, 3, 4);
+            cmdCancelar.Name = "cmdCancelar";
+            cmdCancelar.Size = new Size(138, 40);
+            cmdCancelar.TabIndex = 13;
+            cmdCancelar.Text = "CANCELAR";
+            cmdCancelar.UseVisualStyleBackColor = true;
+            cmdCancelar.Click += cmdCancelar_Click;
             // 
             // frmRegistro
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(txtCantidad);
+            ClientSize = new Size(562, 540);
+            Controls.Add(cmdCancelar);
+            Controls.Add(lblResultados);
+            Controls.Add(mtbPrecio);
+            Controls.Add(nudCantidad);
             Controls.Add(cmdAceptar);
-            Controls.Add(txtPrecioUnitario);
             Controls.Add(lstProductos);
             Controls.Add(dtpFecha);
             Controls.Add(lblTitulo);
@@ -146,8 +180,10 @@
             Controls.Add(lblCantidad);
             Controls.Add(lblPrecioUnitario);
             Controls.Add(lblFecha);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "frmRegistro";
             Text = "Registro";
+            ((System.ComponentModel.ISupportInitialize)nudCantidad).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -161,9 +197,10 @@
         private Label lblTitulo;
         private DateTimePicker dtpFecha;
         private ComboBox lstProductos;
-        private NumericUpDown numericUpDown1;
-        private TextBox txtPrecioUnitario;
+        private NumericUpDown nudCantidad;
         private Button cmdAceptar;
-        private TextBox txtCantidad;
+        private MaskedTextBox mtbPrecio;
+        private Label lblResultados;
+        private Button cmdCancelar;
     }
 }
--- pryDelgadoAppRegistro/frmRegistro.cs
+++ pryDelgadoAppRegistro/frmRegistro.cs
@@ -6,5 +6,78 @@ namespace pryDelgadoAppRegistro
         {
             InitializeComponent();
         }
+        DateTime vFecha = DateTime.Now;
+        String vProducto = "";
+        int vCantidad = 0;
+        int vPrecioU = 0;
+
+
+
+        private void lstProductos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstProductos.Text != "")
+            {
+                nudCantidad.Enabled = true;
+            }
+            else
+            {
+                nudCantidad.Enabled = false;
+            }
+        }
+
+        private void nudCantidad_ValueChanged(object sender, EventArgs e)
+        {
+            if (nudCantidad.Value > 0)
+            {
+                mtbPrecio.Enabled = true;
+            }
+            else
+            {
+                mtbPrecio.Enabled = false;
+            }
+        }
+
+        private void mtbPrecio_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+            if (mtbPrecio.Text != "")
+            {
+                cmdAceptar.Enabled = true;
+            }
+            else
+            {
+                cmdAceptar.Enabled = false;
+            }
+        }
+
+        private void cmdAceptar_Click(object sender, EventArgs e)
+        {
+            vFecha = dtpFecha.Value;
+            vProducto = lstProductos.Text;
+            vCantidad = Convert.ToInt32(nudCantidad.Value);
+            vPrecioU = Convert.ToInt32(mtbPrecio.Text);
+
+
+            lblResultados.Text += (vFecha + " " + vProducto + " " + vCantidad + " " + vPrecioU);
+
+            LimpiarControles();
+
+        }
+
+        private void LimpiarControles()
+        {
+            dtpFecha.Value = DateTime.Now;
+            lstProductos.SelectedIndex = -1;
+            nudCantidad.Value = 0;
+            mtbPrecio.Text = "";
+            lstProductos.Focus();
+        }
+
+        private void cmdCancelar_Click(object sender, EventArgs e)
+        {
+              if (MessageBox.Show("¿Desea salir del programa?", "Salir", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+      {
+          this.Close();
+      }
+        }
     }
 }
